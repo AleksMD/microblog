372f893e6b6dac9da9bb3105aeae0f5d4d930015
AleksMD <budjonniy291@gmail.com>
2019-07-05 22:05:47 +0300

    Applied followers support

--- app/models.py
+++ app/models.py
@@ -13,6 +13,11 @@ now = datetime.utcnow # is needed for further representation
 from hashlib import md5 # for generating and obtaining user avatar
 
 
+followers = db.Table('followers',#association table for many-to-many relationship in "User" table
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+	)
+
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
@@ -21,6 +26,12 @@ class User(UserMixin, db.Model):
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+	followed = db.relationship('User', secondary=followers,
+	 	primaryjoin=(followers.c.follower_id == id),
+	 	secondaryjoin=(followers.c.followed_id == id),
+	 	backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+
 
 	def __repr__(self):
 		return 'User {}'.format(self.username)
@@ -40,6 +51,25 @@ class User(UserMixin, db.Model):
 		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
 			digest, size)
 
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+
+	def is_following(self, user):
+		return self.followed.filter(
+			followers.c.followed_id == user.id).count() > 0 #built-in method in SQLAlchemy
+
+	def followed_posts(self):#get post of followed user
+		followed = Post.query.join(
+			followers, (followers.c.followed_id == Post.user_id)).filter(
+				followers.c.follower_id == self.id)
+		own = Post.query.filter_by(user_id =self.id)
+		return followed.union(own).order_by(Post.timestamp.desc())
+
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.String(140))
@@ -48,3 +78,5 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return 'Post {}'.format(self.body)
+
+
